248b7976ed229392aa05e5ab34322c78874139e3
Farheen <farheen.s.shaikh05@gmail.com>
2026-09-24 01:30:09 -0700

    Time: 3 ms (74.59%), Space: 69.8 MB (25.8%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,13 +1,13 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         int windowSum = 0;
-
-        for(int i = 0; i < k; i++){
+        // initial windows
+        for(int i = 0; i < k; i++){ // 
             windowSum +=  nums[i];
         }
 
         int maxSum = windowSum;
-
+        // Slide the window
         for(int i = k; i < nums.length; i++){
 
             windowSum += nums[i] - nums[i - k];
